2cfc2fe88296542b29cc454e88bd6c5be49fc580
Hellowworld-Marcus <vinny.helloworld@gmail.com>
2022-04-04 13:36:03 -0300

    section about

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
+// usando classes para mostrar o menu, por padrao ecsondido
+
 var hamburguer = document.querySelector(".hamburguer");
 
 hamburguer.addEventListener("click", function(){
-    document.querySelector(".container").classList.toggle("show-menu");
+    document.querySelector(".container").classList.toggle("show-menu"); // o metodo toggle coloca ou retira da div  .container a classe .show-menu, a qual as configurações de apareceer estao atreladas .  
 });
+
 
 
