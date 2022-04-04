df7545ae71f9b2e3c0c8d39558f0cb3247db4fcc
Hellowworld-Marcus <vinny.helloworld@gmail.com>
2022-04-04 06:49:25 -0300

    sidebar

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu");
+});
+
+
