c75d2a5cecec1ae07306ef2fd322fb0b3ea8836d
Hellowworld-Marcus <vinny.helloworld@gmail.com>
2022-04-05 08:20:03 -0300

    lampada

---
+++ lamp.js
@@ -0,0 +1,11 @@
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+let lamp = document.getElementById('lamp');
+
+turnOn.addEventListener('click', function(){
+    lamp.src = './imagens/acesa.jpg'
+})
+
+turnOff.addEventListener('click', function(){
+    lamp.src = './imagens/desligada.jpg'
+})
