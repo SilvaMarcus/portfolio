981b29ece9b2d38eb29500c8b77bc7ac636b0ede
Hellowworld-Marcus <vinny.helloworld@gmail.com>
2022-04-06 03:01:13 -0300

    calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,9 +1,19 @@
 
 const display = document.getElementById('display');
+const numeros = document.querySelectorAll('[id*=tecla]'); /* id*=tecla  significa que todo id que tiver parte do nome tecla vai ser capturado, ou seja, todo ID que contenha tecla em seu nome mesmo tecla1 tecla2 tecla3 ....  criou uma node list */  
 
-const numeros = document.querySelectorAll('[id*=tecla]') /* id*=tecla  significa que todo id que tiver parte do nome tecla vai ser capturado */
+/*  ============= numeros é uma node list  =======*/
 
-const inserirNumero = (evento)  => display.textContent = evento.target.textContent;
 
-numeros.forEach (numero => numero.addEventListener('click', inserirNumero));
+const inserirNumero = (evento) => {
+    display.textContent = evento.target.textContent 
+    
+    // a funcao esta inserindo no textcontent do display a informacao de que , o evento do click, cujo alvo esta no conteudo do texto da div,  vai aparecer no display. 
+
+}
+
+numeros.forEach(numero => numero.addEventListener('click',inserirNumero)); /*  a node list de numeros NUMEROS, esta recebendo uma forEach, para cada item da lista foi adicionado um addEventListener, que vai acionar a callback  => inserirNumero() */
+
+
+
 
