17f525a7916da239235c0484141a1eb8109556e0
Hellowworld-Marcus <vinny.helloworld@gmail.com>
2022-04-06 06:56:01 -0300

    calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,19 +1,143 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector("[data-previous-operand]");
 
-const display = document.getElementById('display');
-const numeros = document.querySelectorAll('[id*=tecla]'); /* id*=tecla  significa que todo id que tiver parte do nome tecla vai ser capturado, ou seja, todo ID que contenha tecla em seu nome mesmo tecla1 tecla2 tecla3 ....  criou uma node list */  
-
-/*  ============= numeros é uma node list  =======*/
-
-
-const inserirNumero = (evento) => {
-    display.textContent = evento.target.textContent 
-    
-    // a funcao esta inserindo no textcontent do display a informacao de que , o evento do click, cujo alvo esta no conteudo do texto da div,  vai aparecer no display. 
-
-}
-
-numeros.forEach(numero => numero.addEventListener('click',inserirNumero)); /*  a node list de numeros NUMEROS, esta recebendo uma forEach, para cada item da lista foi adicionado um addEventListener, que vai acionar a callback  => inserirNumero() */
-
-
-
-
+const currentOperandTextElement = document.querySelector(
+    "[data-current-operand]"
+  );
+  
+  class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement;
+      this.currentOperandTextElement = currentOperandTextElement;
+      this.clear();
+    }
+  
+    formatDisplayNumber(number) {
+      const stringNumber = number.toString();
+  
+      const integerDigits = parseFloat(stringNumber.split(".")[0]);
+      const decimalDigits = stringNumber.split(".")[1];
+  
+      let integerDisplay;
+  
+      if (isNaN(integerDigits)) {
+        integerDisplay = "";
+      } else {
+        integerDisplay = integerDigits.toLocaleString("en", {
+          maximumFractionDigits: 0,
+        });
+      }
+  
+      if (decimalDigits != null) {
+        return `${integerDisplay}.${decimalDigits}`;
+      } else {
+        return integerDisplay;
+      }
+    }
+  
+    delete() {
+      this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+  
+    calculate() {
+      let result;
+  
+      const _previousOperand = parseFloat(this.previousOperand);
+      const _currentOperand = parseFloat(this.currentOperand);
+  
+      if (isNaN(_previousOperand) || isNaN(_currentOperand)) return;
+  
+      switch (this.operation) {
+        case "+":
+          result = _previousOperand + _currentOperand;
+          break;
+        case "-":
+          result = _previousOperand - _currentOperand;
+          break;
+        case "÷":
+          result = _previousOperand / _currentOperand;
+          break;
+        case "*":
+          result = _previousOperand * _currentOperand;
+          break;
+        default:
+          return;
+      }
+  
+      this.currentOperand = result;
+      this.operation = undefined;
+      this.previousOperand = "";
+    }
+  
+    chooseOperation(operation) {
+      if (this.currentOperand === "") return;
+  
+      if (this.previousOperand !== "") {
+        this.calculate();
+      }
+  
+      this.operation = operation;
+  
+      this.previousOperand = this.currentOperand;
+      this.currentOperand = "";
+    }
+  
+    appendNumber(number) {
+      if (this.currentOperand.includes(".") && number === ".") return;
+  
+      this.currentOperand = `${this.currentOperand}${number.toString()}`;
+    }
+  
+    clear() {
+      this.currentOperand = "";
+      this.previousOperand = "";
+      this.operation = undefined;
+    }
+  
+    updateDisplay() {
+      this.previousOperandTextElement.innerText = `${this.formatDisplayNumber(
+        this.previousOperand
+      )} ${this.operation || ""}`;
+      this.currentOperandTextElement.innerText = this.formatDisplayNumber(
+        this.currentOperand
+      );
+    }
+  }
+  
+  const calculator = new Calculator(
+    previousOperandTextElement,
+    currentOperandTextElement
+  );
+  
+  for (const numberButton of numberButtons) {
+    numberButton.addEventListener("click", () => {
+      calculator.appendNumber(numberButton.innerText);
+      calculator.updateDisplay();
+    });
+  }
+  
+  for (const operationButton of operationButtons) {
+    operationButton.addEventListener("click", () => {
+      calculator.chooseOperation(operationButton.innerText);
+      calculator.updateDisplay();
+    });
+  }
+  
+  allClearButton.addEventListener("click", () => {
+    calculator.clear();
+    calculator.updateDisplay();
+  });
+  
+  equalsButton.addEventListener("click", () => {
+    calculator.calculate();
+    calculator.updateDisplay();
+  });
+  
+  deleteButton.addEventListener("click", () => {
+    calculator.delete();
+    calculator.updateDisplay();
+  });
